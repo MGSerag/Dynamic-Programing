e74b5c7d0fec2920060fc21094f7b86df6bc3c45
MGSerag <MG.Serag@gmail.com>
2022-07-10 23:31:58 +0200

    Create bestSum.py

---
+++ 10- How Sum/bestSum.py
@@ -0,0 +1,31 @@
+from itertools import combinations
+
+
+memo = {}
+
+def bestSum(targetSum, numbers):
+
+    if targetSum in memo: return memo[targetSum]        
+    if targetSum == 0: return []
+    if targetSum < 0: return None
+
+    shortestCombination = None
+
+    for n in numbers:
+        remainder = targetSum - n
+        remainderCombination = bestSum(remainder, numbers)
+        
+        if remainderCombination != None:
+            combination = remainderCombination.copy()
+            combination.append(n)
+
+            if shortestCombination is None or len(combination) < len(shortestCombination):
+                shortestCombination = combination
+
+    memo[targetSum] = shortestCombination
+    return shortestCombination
+
+targetSum = 15
+numbers = [1, 5, 7]
+
+print(bestSum(targetSum, numbers))
